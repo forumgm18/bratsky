0d196a13395ba737cafa74387484fbebb766364c
Lyrklif <41914434+Lyrklif@users.noreply.github.com>
2019-05-14 00:12:18 +0400

    личный кабинет верстка

--- assets/src/js/other/personal_account-script.js
+++ assets/src/js/other/personal_account-script.js
@@ -1,7 +1,7 @@
 // Открытие спойлера таблицы 
 $(document).ready(function () {
-	$('.history__spoiler-body').hide()
-	$('.history__spoiler-btn').click(function () {
+	$('.table__spoiler-body').hide()
+	$('.table__spoiler-btn').click(function () {
 		$(this).parent().parent().toggleClass("folded").toggleClass("unfolded").next().toggle('normal');
 		$(this).parent().parent().toggleClass("folded").toggleClass("unfolded").next().next().toggle('normal');
 	})
