047cff0e17b8758b61ddc082d52c244be6485935
forumgm18 <forumgm18@gmail.com>
2019-06-15 19:31:48 +0400

    форма обратной связи

--- assets/build/js/other/personal_account-script.js
+++ assets/build/js/other/personal_account-script.js
@@ -4,17 +4,26 @@ $(document).ready(function () {
 	$('.table__spoiler-btn').click(function () {
 		$(this).parent().parent().toggleClass("folded").toggleClass("unfolded").next().toggle('normal');
 		$(this).parent().parent().toggleClass("folded").toggleClass("unfolded").next().next().toggle('normal');
-	})
-});
+	});
 
 // При нажатии на кнопку "Изменить данные":
 // - скрыть данные о пользователе,
 // - показать форму с полями для заполнения личных данных,
 // - добавить возможность загрузить фото на аватар (показать label)
-$(document).ready(function () {
 	$('#SHOW-EDIT-MY-DATA').click(function () {
 		$('.my-data__details').css("display", "none");
-		$('.my-data__form').css("display", "grid");
+		$('.my-data__form').css("display", "flex");
 		$('.my-data__file-photo').css("display", "block");
-	})
+	});
+
+$(".personal-menu__item").on('click',function(e){
+	var lab=$(this).attr("data-label");
+	var dl= "[data-label=" + lab + "]";
+	$('section '+dl).addClass('d-block').siblings().removeClass('d-block');
+	$('h1 '+dl).addClass('d-block').siblings().removeClass('d-block');
+	$(this).addClass('active').siblings().removeClass('active');;
+
+
+});
+
 });
